b6e867de3809865b53f28810cc45f868fc7c9dca
anandhkrishna <45140249+anandhkrishna@users.noreply.github.com>
2020-02-21 16:31:04 +0530

    Add files via upload

---
+++ concatination on string/conc.pl
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+use 5.010;
+my $word='MNIDDKL';
+if($word eq 'QSTVSGE')
+{
+	print "QSTVSGE\n";
+}
+elsif($word eq 'MRQQDMISHDEL')
+{	
+	print"MRQQDMISHIDEL";
+}
+elsif($word eq 'MNIDDKL')
+{
+	print"MNIDDKL";
+}
+else
+{
+	print "is\"$word\"a peptide?.the program was not sure\n";
+}

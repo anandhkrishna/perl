127e5040ab9e4abd4fa5e752f35add0bf98d3255
anandhkrishna <45140249+anandhkrishna@users.noreply.github.com>
2020-02-21 16:15:15 +0530

    Add files via upload

---
+++ string repalce with another string/replace_string.pl
@@ -0,0 +1,7 @@
+
+my $var1 = "Tigers are big and frightening.";
+$var1 =~ s/Tigers/Lions/;   
+print "$var1\n";   
+my $var2 = "Red roses are very popular. Yellow roses are less seen.";   
+$var2 =~ s/roses/flowers/g;   
+print "$var2\n";
